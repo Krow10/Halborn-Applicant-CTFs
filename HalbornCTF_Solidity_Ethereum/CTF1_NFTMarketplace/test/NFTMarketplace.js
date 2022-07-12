ecc1758ac955a60aa55e3502c81734c2609f7aee
Donneger Etienne <23462475+Krow10@users.noreply.github.com>
2022-07-12 12:22:12 -0400

    Setup contracts factories for test

--- HalbornCTF_Solidity_Ethereum/CTF1_NFTMarketplace/test/NFTMarketplace.js
+++ HalbornCTF_Solidity_Ethereum/CTF1_NFTMarketplace/test/NFTMarketplace.js
@@ -3,7 +3,11 @@ const { expect } = require('chai');
 
 describe('[NFTMarketplace]', function() {
 	before(async function() {
+		[deployer, governance, attacker] = await ethers.getSigners();
 
+		this.token = await (await ethers.getContractFactory('ApeCoin', deployer)).deploy();
+		this.nfts = await (await ethers.getContractFactory('HalbornNFT', deployer)).deploy();
+		this.marketplace = await (await ethers.getContractFactory('NFTMarketplace', deployer)).deploy(governance.address, this.token.address, this.nfts.address);
 	});
 
 	it('Test', async function() {
