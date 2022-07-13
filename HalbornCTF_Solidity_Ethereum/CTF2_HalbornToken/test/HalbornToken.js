fba27627889042965f5fb9621d4e97d53e6492d5
Donneger Etienne <23462475+Krow10@users.noreply.github.com>
2022-07-13 11:58:16 -0400

    Refactor for Halborn scenario setup

--- HalbornCTF_Solidity_Ethereum/CTF2_HalbornToken/test/HalbornToken.js
+++ HalbornCTF_Solidity_Ethereum/CTF2_HalbornToken/test/HalbornToken.js
@@ -2,11 +2,13 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
 describe('[HalbornToken] Exploits', function() {
-	let attacker;
+	let attacker, employees;
 
 	beforeEach(async function() {
-		[deployer, whitelisted, attacker] = await ethers.getSigners();
+		[steve, alice, bob, charlie, david, whitelisted, attacker] = await ethers.getSigners();
+		employees = [alice, bob, charlie, david];
 
+		// Setup a whitelisted address for testing 'validity' and 'mintTokensWithWhitelist' functions
 		let whitelistedHash = ethers.utils.solidityKeccak256(['address'], [whitelisted.address]);
 		
 		// Create bytes32 proof from a "password" 
@@ -15,22 +17,44 @@ describe('[HalbornToken] Exploits', function() {
 		// Root is keccak256 of the whitelisted address hash and "password" proof that will pass the 'verify' function
 		let root = ethers.utils.solidityKeccak256(
 			['bytes32', 'bytes32'], 
-			ethers.BigNumber.from(whitelistedHash) > ethers.BigNumber.from(proof) ? [proof, whitelistedHash] : [whitelistedHash, proof] // Check hashing order for MerkleTree
+			ethers.BigNumber.from(whitelistedHash) <= ethers.BigNumber.from(proof) ? [proof, whitelistedHash] : [whitelistedHash, proof] // Check hashing order for MerkleTree
 		);
 		// console.log("[JS]\nhash:", whitelistedHash, '\nproof:', proof, '\nroot:', root);
 
-		let deployerFunds = ethers.utils.parseEther('100000');
-		this.token = await (await ethers.getContractFactory('HalbornToken', deployer)).deploy(
+		// Steve deploy the contract
+		let deployerFunds = ethers.utils.parseEther('10000');
+		this.token = await (await ethers.getContractFactory('HalbornToken', steve)).deploy(
 			"HalbornToken", // Name
 			"HAL", // Symbol
-			deployerFunds, // Minted amount for deployer 
-			deployer.address, // Deployer address
+			deployerFunds, // Minted amount for deployer Steve
+			steve.address, // Deployer address
 			root
 		);
 
 		expect(
-			await this.token.balanceOf(deployer.address)
+			await this.token.balanceOf(steve.address)
 		).to.be.equal(deployerFunds); // Check deployer minted tokens
+
+		// Transfer 100 tokens to each employees
+		let employeeFunds = ethers.utils.parseEther('100');
+		for (let i = 0; i < employees.length; ++i){
+			let now = (await ethers.provider.getBlock(await ethers.provider.getBlockNumber())).timestamp;
+			await (await this.token.connect(steve)).transfer(employees[i].address, employeeFunds);
+			await (await this.token.connect(employees[i])).newTimeLock(
+				employeeFunds,
+				now + 60, // vestTime
+				now + 3600*24*30*6, // cliffTime ~ 6 months from now
+				now + 3600*24*30*12 // disbursementPeriod ~ 1 year from now
+			);
+
+			expect(
+				await this.token.balanceOf(employees[i].address)
+			).to.be.equal(employeeFunds); // Checks balance of employee is 100 tokens 
+
+			await expect(
+				(await this.token.connect(employees[i])).transfer(steve.address, employeeFunds)
+			).to.be.reverted; // Checks that tokens are locked
+		}
 
 		let whitelistedFunds = ethers.utils.parseEther('10');
 		await (await this.token.connect(whitelisted)).mintTokensWithWhitelist(whitelistedFunds, root, [proof]); // Check validity of root + proof
