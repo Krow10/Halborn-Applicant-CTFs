f1611801e37c29fddf56bdd8602d0e361fe00971
Donneger Etienne <23462475+Krow10@users.noreply.github.com>
2022-07-12 22:03:11 -0400

    Cleanup dependencies and setup CTF2_HalbornToken

---
+++ HalbornCTF_Solidity_Ethereum/CTF2_HalbornToken/report.md
@@ -0,0 +1,14 @@
+# Analysis of HalbornToken contract
+
+## Table of Contents
+1. [Title](#1-Title)
+
+## 1. Title
+### Description
+
+### Code
+
+### Recommendations
+**Immediate:**
+
+**Future:**
---
+++ HalbornCTF_Solidity_Ethereum/CTF2_HalbornToken/test/HalbornToken.js
@@ -0,0 +1,12 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+describe('[HalbornToken] Exploits', function() {
+	beforeEach(async function() {
+
+	});
+
+	it('', async function() {
+
+	});
+});
